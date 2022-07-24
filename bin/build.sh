a54b32ba23a90d514b6ebdd9a39d4f01b16ec9de
hamzadis <adis@hamzadis.com>
2022-07-24 21:01:29 +0200

    Changed to unpatched rom

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh Palit.GTX650.1024.120917.patched.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh Palit.GTX650.1024.120917.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
