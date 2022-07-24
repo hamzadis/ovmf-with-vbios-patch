02a11eb6d9003c184d3f19ee8f737c93dc1d04ed
hamzadis <adis@hamzadis.com>
2022-07-24 20:33:24 +0200

    Changed rom target

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh gtx_1050_ti_mobile.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh Palit.GTX650.1024.120917.patched.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
